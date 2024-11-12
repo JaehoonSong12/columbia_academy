196693fd16f85e2904f79d241a4a3b2993cf2a94
JaehoonSong12 <manual20151276@gmail.com>
2024-11-11 21:12:49 -0500

    11/11/2024 updated by Jaehoon

--- src/app.py
+++ src/app.py
@@ -13,6 +13,53 @@ f.close()  # Close the file after reading
 
 
 
+# variable define (syntax)
+myVar = "Hello World World"
+
+# API define (syntax)
+def myAPI(mycomment):
+    print("You are calling me, but you don't need to know anything (abstraction)")
+    print(mycomment)
+
+
+
+
+
+# API is the core concept for abstraction
+# abstraction means hiding all the actions (semantics 
+# by just calling the API, offers simpler syntax.)
+
+# (1) manual coding
+print("You are calling me, but you don't need to know anything (abstraction)")
+print("Hello World World")
+
+# (2) API call (syntax)
+myAPI(myVar) # version 1: using variable, you can bring up data on your RAM
+myAPI("my data I'm directly writing") # version 2: not using varible, temproray use (hard-code)
+# "Don't do hardcode!"
+
+# API - general term for programming.
+# function - a special name indicating "API" role in python programming language
+
+
+
+
+
+# syntax for python: <target>(<arguments>)        
+# syntax for bash: <target> <arguments>        
+#                                       <- very like English or Korean ... grammer
+#                                       <- a text rules you have to write for running something 
+### target == print
+### arguments == ["Hello World"]
+### 
+
+# semantics: how your memory is going to work based on your given syntax
+
+## Syntax -> command, semantics -> behavior (is the one that is abstracted out.)
+
+
+
+
 
 
 #########################################
