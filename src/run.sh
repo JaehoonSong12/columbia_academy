acbe40ecfb58b75be226d85a4f2beb211bc0f50c
JaehoonSong12 <manual20151276@gmail.com>
2024-11-16 02:57:27 -0500

    Normal Execution commit: 2024-11-16_02-57-27 updated by Jaehoon

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+EXECUTABLE="app"                                #### YOUR DATA HERE
+############################################
+########## Shell Script (Scripts) ##########
+############################################
 pip install pipreqs
 pipreqs . --force
 pip install -r requirements.txt
+clear
+python "${EXECUTABLE}.py"
