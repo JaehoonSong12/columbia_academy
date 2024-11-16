3d5c7019df06a8612f1c2b9288e06ada6a29fb8e
JaehoonSong12 <manual20151276@gmail.com>
2024-11-16 03:07:06 -0500

    Normal Execution commit: 2024-11-16_03-07-06 updated by Jaehoon

--- src/app.py
+++ src/app.py
@@ -1,78 +1,21 @@
-# Open the file in write mode and write some strings
-f = open('myfile.txt', 'w')
-f.write("Hello, this is the first line.\n")
-f.write("This is the second line.\n")
-f.write("And this is the third line.\n")
-f.close()  # Close the file after writing
+# app.py
+import hw00
 
-# Open the file in read mode and print its contents
-f = open('myfile.txt', 'r')
-content = f.read()  # Read the entire file
-print(content)
-f.close()  # Close the file after reading
+def cli():
+    print("App is running...")
+    hw00.main()  # Test HW00
+    # hw01.main()  # Test HW01
+    # hw02.main()  # Test HW02
+    # hw03.main()  # Test HW03
+    # hw04.main()  # Test HW04
+    # hw05.main()  # Test HW05
+    # hw06.main()  # Test HW06
+    # hw07.main()  # Test HW07
+    # hw08.main()  # Test HW08
+    # hw09.main()  # Test HW09
+    # hw10.main()  # Test HW10
+    # hw11.main()  # Test HW11
+    # hw12.main()  # Test HW12
 
-
-
-# variable define (syntax)
-myVar = "Hello World World"
-
-# API define (syntax)
-def myAPI(mycomment):
-    print("You are calling me, but you don't need to know anything (abstraction)")
-    print(mycomment)
-
-
-
-
-
-# API is the core concept for abstraction
-# abstraction means hiding all the actions (semantics 
-# by just calling the API, offers simpler syntax.)
-
-# (1) manual coding
-print("You are calling me, but you don't need to know anything (abstraction)")
-print("Hello World World")
-
-# (2) API call (syntax)
-myAPI(myVar) # version 1: using variable, you can bring up data on your RAM
-myAPI("my data I'm directly writing") # version 2: not using varible, temproray use (hard-code)
-# "Don't do hardcode!"
-
-# API - general term for programming.
-# function - a special name indicating "API" role in python programming language
-
-
-
-
-
-# syntax for python: <target>(<arguments>)        
-# syntax for bash: <target> <arguments>        
-#                                       <- very like English or Korean ... grammer
-#                                       <- a text rules you have to write for running something 
-### target == print
-### arguments == ["Hello World"]
-### 
-
-# semantics: how your memory is going to work based on your given syntax
-
-## Syntax -> command, semantics -> behavior (is the one that is abstracted out.)
-
-
-
-
-
-
-#########################################
-
-"""
-Function Name: listen()
-Parameters: N/A
-Returns: None
-"""
-def listen():
-    songs = int(input("How many songs did you listen to? "))
-    podcasts = int(input("How many podcasts did you listen to? "))
-    totalMinutes = songs * 3 + podcasts * 25
-    print("By listening to " + str(songs) + " songs and " + str(podcasts) + " podcasts, you have spent " + str(totalMinutes // 60) + " hours and " + str(totalMinutes % 60) + " minutes on Spotify.")
-
-#########################################
+if __name__ == "__main__":
+    cli()
---
+++ src/hw00.py
@@ -0,0 +1,10 @@
+# hw00.py
+
+def greet():
+    return "Hello from hw01 module!"
+
+def main():
+    print(greet())
+
+if __name__ == "__main__":
+    main()
