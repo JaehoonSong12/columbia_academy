f83e5534a0ff7fb751bb25de3c5c425dcaa78580
JaehoonSong12 <manual20151276@gmail.com>
2024-11-16 03:00:59 -0500

    Normal Execution commit: 2024-11-16_03-00-59 updated by Jaehoon

--- src/run.sh
+++ src/run.sh
@@ -3,8 +3,15 @@ EXECUTABLE="app"                                #### YOUR DATA HERE
 ############################################
 ########## Shell Script (Scripts) ##########
 ############################################
+## python-env_setup
 pip install pipreqs
 pipreqs . --force
 pip install -r requirements.txt
 clear
-python "${EXECUTABLE}.py"
+## python-execution
+if command -v python &>/dev/null; then # Check for Python command (python3 or python)
+    PYTHON_CMD=python
+elif command -v python3 &>/dev/null; then
+    PYTHON_CMD=python3
+fi
+$PYTHON_CMD "${EXECUTABLE}.py"
