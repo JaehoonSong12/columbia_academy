b105524189f196aefd6bd3775128f637b9b3226d
JaehoonSong12 <manual20151276@gmail.com>
2024-11-17 03:03:34 -0500

    Normal Execution commit: 2024-11-17_03-03-34 updated by Jaehoon

--- src/app.py
+++ src/app.py
@@ -17,18 +17,18 @@ def cli():
     # hw11.main()  # Test HW11
     # hw12.main()  # Test HW12
     
-    gt_hw01.HW01.bathBomb()  # Test HW01
-    # hw02.main()  # Test HW02
-    # hw03.main()  # Test HW03
-    # hw04.main()  # Test HW04
-    # hw05.main()  # Test HW05
-    # hw06.main()  # Test HW06
-    # hw07.main()  # Test HW07
-    # hw08.main()  # Test HW08
-    # hw09.main()  # Test HW09
-    # hw10.main()  # Test HW10
-    # hw11.main()  # Test HW11
-    # hw12.main()  # Test HW12
+    gt_hw01.HW01.bathBomb()  # Test GT_HW01
+    # hw02.main()  # Test GT_HW02
+    # hw03.main()  # Test GT_HW03
+    # hw04.main()  # Test GT_HW04
+    # hw05.main()  # Test GT_HW05
+    # hw06.main()  # Test GT_HW06
+    # hw07.main()  # Test GT_HW07
+    # hw08.main()  # Test GT_HW08
+    # hw09.main()  # Test GT_HW09
+    # hw10.main()  # Test GT_HW10
+    # hw11.main()  # Test GT_HW11
+    # hw12.main()  # Test GT_HW12
 
 if __name__ == "__main__":
     cli()
