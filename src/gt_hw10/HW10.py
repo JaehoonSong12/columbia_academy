81b8952e2a007afb730fd5b87a81ea73061740b5
JaehoonSong12 <manual20151276@gmail.com>
2025-03-13 21:03:55 -0400

    Normal Execution commit: 2025-03-13_21-03-54 updated by Jaehoon

--- src/gt_hw10/HW10.py
+++ src/gt_hw10/HW10.py
@@ -27,55 +27,24 @@ class Task:
 
 class Crewmate:
     def __init__(self, name, color, accessories = ()):
-        self.name = name
-        self.color = color
-        self.accessories = accessories
-        self.isAlive = True
-        self.tasksDone = 0
+        # [your implementation]
+        return
 
     def doTask(self, Task):
-        if Task.isCompleted:
-            return "Nothing to do here."
-        Task.isCompleted = True
-        self.tasksDone += 1
+        # [your implementation]
+        return None
 
     def vote(self, AmongUs):
-        for x in AmongUs.crewmates:
-	        if self.name[0] == x.name[0] and self.name != x.name and x.isAlive:
-		        return x
-        for x in AmongUs.impostors:
-            if self.name[0] == x.name[0] and self.name != x.name and x.isAlive:
-                return x
+        # [your implementation]
+        # Default: vote for self if no candidate matches.
+        return self
 
     def __repr__(self): # provided
         return "Crewmate(name: {}, color: {})".format(self.name, self.color)
 
     def callMeeting(self, AmongUs):
-        votes = {}
-        for x in AmongUs.crewmates:
-            theVote = x.vote(AmongUs).name
-            if theVote in votes:
-                votes[theVote] = votes[theVote] + 1
-            else:
-                votes[theVote] = 1
-        for x in AmongUs.impostors:
-            theVote = x.vote(AmongUs).name
-            if theVote in votes:
-                votes[theVote] = votes[theVote] + 1
-            else:
-                votes[theVote] = 1
-        highest = None
-        for x in votes:
-          if highest == None or votes[x] > votes[highest]:
-            highest = x
-        for x in AmongUs.crewmates:
-          if x.name == highest:
-            x.isAlive = False
-            return 	"{} was not An Impostor.".format(x.name)
-        for x in AmongUs.impostors:
-          if x.name == highest:
-            x.isAlive = False
-            return "{} was An Impostor".format(x.name)
+        # [your implementation]
+        return
         
 
     def __eq__(self, other = False): # provided
@@ -83,25 +52,17 @@ class Crewmate:
 
 class Impostor:
     def __init__(self, name, color, accessories = ()):
-        self.name = name
-        self.color = color
-        self.accessories = accessories
-        self.isAlive = True
-        self.eliminateCount = 0
+        # [your implementation]
+        return
 
     def eliminate(self, person):
-        if type(person) == type(self):
-            return "They're on your team -_-"
-        person.isAlive = False
-        self.eliminateCount += 1
+        # [your implementation]
+        return
 
     def vote(self, AmongUs):
-        for x in AmongUs.crewmates:
-	        if self.name[0] == x.name[0] and self.name != x.name and x.isAlive:
-		        return x
-        for x in AmongUs.impostors:
-            if self.name[0] == x.name[0] and self.name != x.name and x.isAlive:
-                return x
+        # [your implementation]
+        # Default: vote for self if no candidate matches.
+        return self
 
     def __repr__(self): # provided
         return "Impostor(name: {}, color: {})".format(self.name, self.color)
@@ -114,49 +75,19 @@ class Impostor:
 
 class AmongUs:
     def __init__(self, maxPlayers):
-        self.maxPlayers = maxPlayers
-        self.rooms = {}
-        self.crewmates = []
-        self.impostors = []
+        # [your implementation]
+        return
 
     def registerPlayer(self, person):
-        if len(self.crewmates) + len(self.impostors) >= self.maxPlayers:
-            return "Lobby is full."
-        for x in self.crewmates:
-            if person.name == x.name:
-                return "Player with name: {} exists.".format(person.name)
-        for x in self.impostors:
-            if person.name == x.name:
-                return "Player with name: {} exists.".format(person.name)
-        if hasattr(person, 'tasksDone'):
-            self.crewmates.append(person)
-        else:
-            self.impostors.append(person)
+        # [your implementation]
+        return None
 
     def registerTask(self, newTask, newRoom):
-        for x in self.rooms:
-            if newTask in self.rooms[x]:
-                return "This task has already been registered."
-        if newRoom.name in self.rooms:
-          temp = self.rooms[newRoom.name]
-          temp.append(newTask)
-          self.rooms[newRoom.name] = temp
-        else:
-          self.rooms[newRoom.name] = [newTask]
+        # [your implementation]
+        return None
 
     def gameOver(self):
-        aliveCrew = 0
-        for x in self.crewmates:
-            if x.isAlive:
-                aliveCrew += 1
-        if aliveCrew == 0:
-            return "Defeat! All crewmates have been eliminated."
-        aliveImpostors = 0
-        for x in self.impostors:
-            if x.isAlive:
-                aliveImpostors += 1
-        if aliveImpostors == 0:
-            return "Victory! All impostors have been eliminated."
+        # [your implementation]
         return "Game is not over yet!"
 
     def __repr__(self): # provided
