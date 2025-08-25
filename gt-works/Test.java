048fbb1f8ad4d546b04970dd45f31e71158856fe
b3rrier <taiyun0410@gmail.com>
2025-08-24 21:02:58 -0400

    Commit from  on Sun Aug 24 21:02:55 EDT 2025

---
+++ gt-works/Test.java
@@ -0,0 +1,20 @@
+/**
+INSTRUCTIONS: 
+    Part Two: Writing and Compiling a Java Class
+    and
+    Part Three: The `main` Method
+
+COMPILE & EXECUTE & CLEANUP (Java):
+
+     javac  -d out              Test.java       # compile (.java to .class)
+     java           -cp out     Test            # execute (.class to run)
+     rm -rf out/                                # clean up .class files
+    
+ */
+
+public class Test { // Part Two
+    public static void main(String[] args) { // Part Three
+        System.out.println("Hello.");
+    }
+
+}
