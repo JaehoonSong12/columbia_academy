63cab0d0f382405add374052eec0805446fe439c
JaehoonSong12 <manual20151276@gmail.com>
2025-09-14 21:07:05 -0400

    Commit from  on Sun, Sep 14, 2025  9:07:02 PM

--- gt-works/hw01/PrimitiveOperations.java
+++ gt-works/hw01/PrimitiveOperations.java
@@ -1,18 +1,34 @@
-/**
-INSTRUCTIONS: 
-    Homework1 is going on...
-    
-COLLABORATION: 
-    I worked on the assignment alone, using only course-provided materials.
+/*
+INSTRUCTIONS:
+    A PDF file is attached in the same folder.
+
+COLLABORATION STATEMENT:
+    I worked on the homework assignment alone, using only course materials.
+
+CHECKSTYLE:
+     java -jar checkstyle-10.23.0-all.jar -c cs1331.xml hw01/*.java
 
 COMPILE & EXECUTE & CLEANUP (Java):
+     javac  -d out                  hw01/PrimitiveOperations.java   # compile (.java to .class)
+     java           -cp "./out"     PrimitiveOperations             # execute (.class to run)
+     rm -rf out/                                                    # clean up .class files
 
-     javac  -d out                  hw01/PrimitiveOperations.java      # compile (.java to .class)
-     java           -cp "./out"     PrimitiveOperations                # execute (.class to run)
-     rm -rf out/                                        # clean up .class files
-    
+DEPENDENCIES:
+     None
+ */
+
+/**
+ * This class is a classwork for primitive operations.
+ *
+ * @author Taiyun Park
+ * @version 0.0.1
  */
 public class PrimitiveOperations {
+    /**
+     * Main method.
+     *
+     * @param args argument from user input
+     */
     public static void main(String[] args) {
         int spongebobIQ = 5;
         double patrickIQ = 3.5;
