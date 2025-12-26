16203a79cde876b86a20f825fcbc83fa56741d4f
jihoo0905 <jhoochoi522@gmail.com>
2025-12-26 13:07:13 -0500

    Commit from  on Fri, Dec 26, 2025  1:07:11 PM

---
+++ std02-calculator/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "cs50.h"
+#include <stdlib.h>
+
+int rand_range(int min, int max) {
+    return min + rand() % (max - min + 1);
+}
+
+
+
+
+
+
+
+int main(int argc, char* argv[]) {
+    printf("---------Project for Python Library---------: %s\n", argv[0]);
+    for (int i = 1; i < argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+    printf("------------------------------\n");
+}
+
+
+
+
+// floating number addition, subtraction, multiplication, division calculator, data type defined for python binding
+float add(float a, float b) {
+    return a + b;
+}
+
+void intro_message(void) {
+    printf("This is a simple calculator for addition, subtraction, multiplication, and division of floating point numbers.\n");
+}
+
+void quiz_time(void) {
+    // 1. ask user to input an integer
+    // 2. the user should guess what the integer is.
+    // 3. if the user guesses correctly, print "Correct!"
+    // 4. if the user guesses incorrectly, print either "Too high!" or "Too low!" accordingly.
+    int answer = rand_range(1, 100);
+    while (1) {
+        int user_input = get_int("Please enter your number: ");
+        printf("You entered: %d\n", user_input);
+        if (user_input == answer) {
+            printf("You are right!\nbb\n");
+            break;
+        } else if (user_input < answer){
+            printf("Your number is smaller than answer\n");
+        } else if (user_input > answer){
+            printf("Your number is bigger than answer\n");
+        }
+    }
+}
